9852354aea4814930d1ed08f134e2a84b9b6d51c
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-03 19:01:29 +0530

    source of blog add

--- utils/HTMLtoNotionBlocks.ts
+++ utils/HTMLtoNotionBlocks.ts
@@ -3,6 +3,9 @@ import { toMdast } from "hast-util-to-mdast"
 import { toMarkdown } from "mdast-util-to-markdown"
 import { markdownToBlocks } from "@tryfabric/martian"
 
+// https://zirkelc.dev/posts/html-to-notion-blocks
+// Refer to this blog
+
 export const convertHtmlToNotionBlock = (data: string) => {
     const hast = fromHtml(data)
     const mdast = toMdast(hast)
